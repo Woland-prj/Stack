52b74ef4af21b13503beeb870b65831b721508e8
Woland-prj <nzimin93@gmail.com>
2023-09-20 15:40:18 +0300

    LW 3 - Stack

---
+++ Stack.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Stack.h"
+
+int main()
+{
+    int TestArr[] = { 1, 2, 3, 4, 5 };
+    int len = std::end(TestArr) - std::begin(TestArr);
+    Stack St;
+    for (int i = 0; i < len; i++) 
+       St.Push(TestArr[i]);
+    while (!St.isEmpty()) {
+        std::cout << St.Pop() << std::endl;
+    }
+}
